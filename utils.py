f21105aa0fa647727f20512da8f5059a57ed3e3a
MinDeaDBlood <151534222+MinDeaDBlood@users.noreply.github.com>
2024-08-05 03:39:34 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,30 +41,32 @@ DataImage = blockimgdiff.DataImage
 try:
     sys.set_int_max_str_digits(0)
 except AttributeError:
-    ...
-if os.name == 'nt':
-    prog_path = getcwd()
-else:
-    prog_path = os.path.normpath(os.path.abspath(os.path.dirname(sys.argv[0])))
-project_name = None
-formats = ([b'PK', "zip"], [b'OPPOENCRYPT!', "ozip"], [b'7z', "7z"], [b'\x53\xef', 'ext', 1080],
-           [b'\x3a\xff\x26\xed', "sparse"], [b'\xe2\xe1\xf5\xe0', "erofs", 1024], [b"CrAU", "payload"],
-           [b"AVB0", "vbmeta"], [b'\xd7\xb7\xab\x1e', "dtbo"], [b'\x10\x20\xF5\xF2', 'f2fs', 1024],
-           [b'\xd0\x0d\xfe\xed', "dtb"], [b"MZ", "exe"], [b".ELF", 'elf'],
-           [b'\x7fELF', 'elf'],
-           [b"ANDROID!", "boot"], [b"VNDRBOOT", "vendor_boot"],
-           [b'AVBf', "avb_foot"], [b'BZh', "bzip2"],
-           [b'CHROMEOS', 'chrome'], [b'\x1f\x8b', "gzip"],
-           [b'\x1f\x9e', "gzip"], [b'\x02\x21\x4c\x18', "lz4_legacy"],
-           [b'\x03\x21\x4c\x18', 'lz4'], [b'\x04\x22\x4d\x18', 'lz4'],
-           [b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\x03', "zopfli"], [b'\xfd7zXZ', 'xz'],
-           [b']\x00\x00\x00\x04\xff\xff\xff\xff\xff\xff\xff\xff', 'lzma'], [b'\x02!L\x18', 'lz4_lg'],
-           [b'\x89PNG', 'png'], [b"LOGO!!!!", 'logo', 4000], [b'\x28\xb5\x2f\xfd', 'zstd'],
-           [b'(\x05\x00\x00$8"%', 'kdz'], [b"\x32\x96\x18\x74", 'dz'], [b'\xcf\xfa\xed\xfe', 'macos_bin'])
+    pass
 
+prog_path = getcwd() if os.name == 'nt' else os.path.normpath(os.path.abspath(os.path.dirname(sys.argv[0])))
+project_name = None
+
+formats = [
+    (b'PK', "zip"), (b'OPPOENCRYPT!', "ozip"), (b'7z', "7z"), 
+    (b'\x53\xef', 'ext', 1080), (b'\x3a\xff\x26\xed', "sparse"), 
+    (b'\xe2\xe1\xf5\xe0', "erofs", 1024), (b"CrAU", "payload"),
+    (b"AVB0", "vbmeta"), (b'\xd7\xb7\xab\x1e', "dtbo"), 
+    (b'\x10\x20\xF5\xF2', 'f2fs', 1024), (b'\xd0\x0d\xfe\xed', "dtb"), 
+    (b"MZ", "exe"), (b".ELF", 'elf'),
+    (b'\x7fELF', 'elf'), (b"ANDROID!", "boot"), 
+    (b"VNDRBOOT", "vendor_boot"), (b'AVBf', "avb_foot"), 
+    (b'BZh', "bzip2"), (b'CHROMEOS', 'chrome'), 
+    (b'\x1f\x8b', "gzip"), (b'\x1f\x9e', "gzip"), 
+    (b'\x02\x21\x4c\x18', "lz4_legacy"), (b'\x03\x21\x4c\x18', 'lz4'), 
+    (b'\x04\x22\x4d\x18', 'lz4'), (b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\x03', "zopfli"), 
+    (b'\xfd7zXZ', 'xz'), (b']\x00\x00\x00\x04\xff\xff\xff\xff\xff\xff\xff\xff', 'lzma'), 
+    (b'\x02!L\x18', 'lz4_lg'), (b'\x89PNG', 'png'), 
+    (b"LOGO!!!!", 'logo', 4000), (b'\x28\xb5\x2f\xfd', 'zstd'), 
+    (b'(\x05\x00\x00\$8"%', 'kdz'), (b"\x32\x96\x18\x74", 'dz'), 
+    (b'\xcf\xfa\xed\xfe', 'macos_bin')
+]
 
 # ----DEFS
-
 
 class Sdat2img:
     def __init__(self, transfer_list_file, new_data_file, output_image_file):
@@ -74,52 +76,51 @@ class Sdat2img:
         self.output_image_file = output_image_file
         self.list_file = self.parse_transfer_list_file()
         block_size = 4096
+        
+        # Извлечение версии
         version = next(self.list_file)
         self.version = version
         next(self.list_file)
+        
         versions = {
             1: "Lollipop 5.0",
             2: "Lollipop 5.1",
             3: "Marshmallow 6.x",
             4: "Nougat 7.x / Oreo 8.x / Pie 9.x",
         }
+        
         print("Android {} detected!\n".format(versions.get(version, f'Unknown version {version}!\n')))
-        # Don't clobber existing files to avoid accidental data loss
+        
+        # Проверка существования выходного файла
+        if exists(self.output_image_file):
+            print(f'Error: the output file "{self.output_image_file}" already exists')
+            print('Remove it, rename it, or choose a different file name.')
+            return
+
         try:
-            output_img = open(self.output_image_file, 'wb')
+            with open(self.output_image_file, 'wb') as output_img, open(self.new_data_file, 'rb') as new_data_file:
+                max_file_size = 0
+                for cmd, block_list in self.list_file:
+                    max_file_size = max(pair[1] for pair in block_list) * block_size
+                    for begin, block_all in block_list:
+                        block_count = block_all - begin
+                        print(f'Copying {block_count} blocks into position {begin}...')
+                        
+                        # Позиция выходного файла
+                        output_img.seek(begin * block_size)
+                        
+                        # Копирование одного блока за раз
+                        while block_count > 0:
+                            output_img.write(new_data_file.read(block_size))
+                            block_count -= 1
+
+                # Увеличение файла, если необходимо
+                if output_img.tell() < max_file_size:
+                    output_img.truncate(max_file_size)
+
+            print(f'Done! Output image: {os.path.realpath(self.output_image_file)}')
         except IOError as e:
-            if e.errno == 17:
-                print(f'Error: the output file "{e.filename}" already exists')
-                print('Remove it, rename it, or choose a different file name.')
-                return
-            else:
-                print(e)
-                return
-
-        new_data_file = open(self.new_data_file, 'rb')
-        max_file_size = 0
-
-        for cmd, block_list in self.list_file:
-            max_file_size = max(pair[1] for pair in block_list) * block_size
-            for begin, block_all in block_list:
-                block_count = block_all - begin
-                print(f'Copying {block_count} blocks into position {begin}...')
-
-                # Position output file
-                output_img.seek(begin * block_size)
-
-                # Copy one block at a time
-                while block_count > 0:
-                    output_img.write(new_data_file.read(block_size))
-                    block_count -= 1
-
-        # Make file larger if necessary
-        if output_img.tell() < max_file_size:
-            output_img.truncate(max_file_size)
-
-        output_img.close()
-        new_data_file.close()
-        print(f'Done! Output image: {os.path.realpath(output_img.name)}')
+            print(e)
 
     @staticmethod
     def rangeset(src):
@@ -128,36 +129,27 @@ class Sdat2img:
         if len(num_set) != num_set[0] + 1:
             print(f'Error on parsing following data to rangeset:\n{src}')
             return
-
         return tuple([(num_set[i], num_set[i + 1]) for i in range(1, len(num_set), 2)])
 
     def parse_transfer_list_file(self):
         with open(self.transfer_list_file, 'r', encoding='utf-8') as trans_list:
-            # First line in transfer list is the version number
-            # Second line in transfer list is the total number of blocks we expect to write
-            if (version := int(trans_list.readline())) >= 2 and (new_blocks := int(trans_list.readline())):
-                # Third line is how many stash entries are needed simultaneously
-                trans_list.readline()
-                # Fourth line is the maximum number of blocks that will be stashed simultaneously
-                trans_list.readline()
-            # Subsequent lines are all individual transfer commands
+            version = int(trans_list.readline())
+            if version >= 2 and (new_blocks := int(trans_list.readline())):
+                trans_list.readline()  # Third line
+                trans_list.readline()  # Fourth line
             yield version
             yield new_blocks
             for line in trans_list:
                 line = line.split(' ')
                 cmd = line[0]
                 if cmd == 'new':
-                    # if cmd in ['erase', 'new', 'zero']:
                     yield [cmd, self.rangeset(line[1])]
-                else:
-                    if cmd in ['erase', 'new', 'zero']:
-                        print(f'Skipping command {cmd}...')
-                        continue
-                    # Skip lines starting with numbers, they are not commands anyway
-                    if not cmd[0].isdigit():
-                        print(f'Command "{cmd}" is not valid.')
-                        return
-
+                elif cmd in ['erase', 'new', 'zero']:
+                    print(f'Skipping command {cmd}...')
+                    continue
+                if not cmd[0].isdigit():
+                    print(f'Command "{cmd}" is not valid.')
+                    return
 
 def gettype(file) -> str:
     """
@@ -204,23 +196,21 @@ def gettype(file) -> str:
         if is_super(file) or is_super2(file):
             return 'super'
     except IndexError:
-        ...
+        pass
+    
     for f_ in formats:
-        if len(f_) == 2:
-            if compare(f_[0]):
-                return f_[1]
-        elif len(f_) == 3:
-            if compare(f_[0], f_[2]):
-                return f_[1]
+        if len(f_) == 2 and compare(f_[0]):
+            return f_[1]
+        elif len(f_) == 3 and compare(f_[0], f_[2]):
+            return f_[1]
+    
     try:
         if LogoDumper(file, str(None)).check_img(file):
             return 'logo'
-    except AssertionError:
-        ...
-    except struct.error:
-        ...
+    except (AssertionError, struct.error):
+        pass
+    
     return "unknown"
-
 
 def dynamic_list_reader(path):
     """
@@ -231,23 +221,20 @@ def dynamic_list_reader(path):
     data = {}
     with open(path, 'r', encoding='utf-8') as l_f:
         for p in l_f.readlines():
-            if p[:1] == '#':
+            if p.startswith('#'):
                 continue
             tmp = p.strip().split()
             if tmp[0] == 'remove_all_groups':
                 data.clear()
             elif tmp[0] == 'add_group':
-                data[tmp[1]] = {}
-                data[tmp[1]]['size'] = tmp[2]
-                data[tmp[1]]['parts'] = []
+                data[tmp[1]] = {'size': tmp[2], 'parts': []}
             elif tmp[0] == 'add':
                 data[tmp[2]]['parts'].append(tmp[1])
     return data
 
-
 def generate_dynamic_list(dbfz, size, set_, lb, work):
     data = ['# Remove all existing dynamic partitions and groups before applying full OTA', 'remove_all_groups']
-    with open(work + "dynamic_partitions_op_list", 'w', encoding='utf-8', newline='\n') as d_list:
+    with open(os.path.join(work, "dynamic_partitions_op_list"), 'w', encoding='utf-8', newline='\n') as d_list:
         if set_ == 1:
             data.append(f'# Add group {dbfz} with maximum size {size}')
             data.append(f'add_group {dbfz} {size}')
@@ -267,14 +254,13 @@ def generate_dynamic_list(dbfz, size, set_, lb, work):
                 data.append(f'add {part}_b {dbfz}_b')
         for part in lb:
             if set_ == 1:
-                data.append(f'# Grow partition {part} from 0 to {os.path.getsize(work + part + ".img")}')
-                data.append(f'resize {part} {os.path.getsize(work + part + ".img")}')
+                data.append(f'# Grow partition {part} from 0 to {os.path.getsize(os.path.join(work, part + ".img"))}')
+                data.append(f'resize {part} {os.path.getsize(os.path.join(work, part + ".img"))}')
             elif set_ in [2, 3]:
-                data.append(f'# Grow partition {part}_a from 0 to {os.path.getsize(work + part + ".img")}')
-                data.append(f'resize {part}_a {os.path.getsize(work + part + ".img")}')
+                data.append(f'# Grow partition {part}_a from 0 to {os.path.getsize(os.path.join(work, part + ".img"))}')
+                data.append(f'resize {part}_a {os.path.getsize(os.path.join(work, part + ".img"))}')
         d_list.writelines([key + "\n" for key in data])
         data.clear()
-
 
 def v_code(num=6) -> str:
     """
@@ -282,16 +268,7 @@ def v_code(num=6) -> str:
     :param num: number of Random Str
     :return:
     """
-    ret = ""
-    for i in range(num):
-        num = randint(0, i)
-        # num = chr(random.randint(48,57))#ASCII表示数字
-        letter = chr(randint(97, 122))  # 取小写字母
-        letter_ = chr(randint(65, 90))  # 取大写字母
-        s = str(choice([num, letter, letter_]))
-        ret += s
-    return ret
-
+    return ''.join(str(choice([randint(0, i), chr(randint(97, 122)), chr(randint(65, 90))])) for i in range(num))
 
 def qc(file_) -> None:
     """
@@ -307,8 +284,6 @@ def qc(file_) -> None:
         f.seek(0)
         f.truncate()
         f.writelines(data)
-    del data
-
 
 def cz(func, *args, join=False):
     """
@@ -323,13 +298,13 @@ def cz(func, *args, join=False):
     if join:
         t.join()
 
-
 def simg2img(path):
     """
     convert Sparse image to Raw Image
     :param path:
     :return:
     """
+    unsparse_file = None
     with open(path, 'rb') as fd:
         if SparseImage(fd).check():
             print('Sparse image detected.')
@@ -338,38 +313,37 @@ def simg2img(path):
             print('Result:[ok]')
         else:
             print(f"{path} not Sparse.Skip!")
+    
     try:
-        if os.path.exists(unsparse_file):
+        if unsparse_file and os.path.exists(unsparse_file):
             os.remove(path)
             os.rename(unsparse_file, path)
     except Exception as e:
         print(e)
 
-
 def img2sdat(input_image, out_dir='.', version=None, prefix='system'):
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
+        
     versions = {
         1: "Android Lollipop 5.0",
         2: "Android Lollipop 5.1",
         3: "Android Marshmallow 6.0",
-        4: "Android Nougat 7.0/7.1/8.0/8.1"}
-    if version not in versions.keys():
-        version = 4
+        4: "Android Nougat 7.0/7.1/8.0/8.1"
+    }
+    
+    version = version if version in versions else 4
     print("Img2sdat(1.7):" + versions[version])
-    blockimgdiff.BlockImageDiff(sparse_img.SparseImage(input_image, tempfile.mkstemp()[1], '0'), None, version).Compute(
-        out_dir + '/' + prefix)
-
+    
+    sparse_image = sparse_img.SparseImage(input_image, tempfile.mkstemp()[1], '0')
+    block_img_diff = blockimgdiff.BlockImageDiff(sparse_image, None, version)
+    block_img_diff.Compute(os.path.join(out_dir, prefix))
 
 def findfile(file, dir_) -> str:
     for root, _, files in os.walk(dir_, topdown=True):
         if file in files:
-            if os.name == 'nt':
-                return (root + os.sep + file).replace("\\", '/')
-            else:
-                return root + os.sep + file
+            return os.path.join(root, file).replace("\\", '/')
     return ''
-
 
 def findfolder(dir__, folder_name):
     """
@@ -384,7 +358,6 @@ def findfolder(dir__, folder_name):
                 return os.path.join(root, dirname).replace("\\", '/')
     return None
 
-
 def jzxs(master):
     """
     Replace Toplevel or Tk to Center
@@ -395,25 +368,18 @@ def jzxs(master):
         f'+{int(master.winfo_screenwidth() / 2 - master.winfo_width() / 2)}+{int(master.winfo_screenheight() / 2 - master.winfo_height() / 2)}')
     master.update()
 
-
 # ----CLASSES
 class LangUtils:
     def __init__(self):
         self.second = {}
 
     def __getattr__(self, item):
-        try:
-            return self.__getattribute__(item)
-        except (AttributeError, ):
-            return self.second.get(item, 'None')
-
+        return self.second.get(item, 'None')
 
 lang = LangUtils()
 
-
 def u64(x):
     return struct.unpack('>Q', x)[0]
-
 
 def payload_reader(payloadfile):
     """
@@ -433,7 +399,6 @@ def payload_reader(payloadfile):
     payloadfile.read(metadata_signature_size)
     dam = um.Metadata(manifest)
     return dam
-
 
 class Vbpatch:
     def __init__(self, file_):
@@ -465,7 +430,6 @@ class Vbpatch:
             return False
         return True
 
-
 class Dumpcfg:
     blksz = 4096
     headoff = 16384
@@ -474,11 +438,9 @@ class Dumpcfg:
     imgblkoffs = []
     imgblkszs = []
 
-
 class Bmphead:
-    def __init__(self, buf: bytes = None):  # Read bytes buf and use this struct to parse
+    def __init__(self, buf: bytes = None):  
         assert buf is not None, f"buf Should be bytes, not {type(buf)}"
-        # print(buf)
         (
             self.magic,
             self.fsize,
@@ -489,11 +451,9 @@ class Bmphead:
             self.height,
         ) = struct.unpack("<H6I", buf)
 
-
 class XiaomiBlkstruct:
     def __init__(self, buf: bytes):
         self.img_offset, self.blksz = struct.unpack("2I", buf)
-
 
 class LogoDumper:
     def __init__(self, img: str, out: str, dir__: str = "pic"):
