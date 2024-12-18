3abc01f6adbfd922a9d28eead782c7a8f9ee6a26
MinDeaDBlood <151534222+MinDeaDBlood@users.noreply.github.com>
2024-12-18 14:49:13 +0300

    Add files via upload

---
+++ src/AI_engine.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2024 The MIO-KITCHEN-SOURCE Project
+#
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html#license-text
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os.path
+from difflib import SequenceMatcher
+from tkinter import Toplevel, ttk, BOTH
+from .utils import move_center, prog_path, lang
+from json import load
+with open(os.path.join(prog_path, 'bin', 'help_document.json'), 'r', encoding='utf-8') as f:
+    library = load(f)
+
+def suggest(string: str = '', language='English', ok='ok'):
+    catch_error = [i for i in string.split("\n") if 'error' in i]
+    if not catch_error:
+        catch_error = [i for i in string.split("\n") if 'failed' in i]
+        if not catch_error:
+            return
+        else:
+            catch_error = catch_error[0]
+    else:
+        catch_error = catch_error[0]
+    if not catch_error:
+        return
+    similarity = 0
+    window = Toplevel()
+    window.resizable(False, False)
+    window.title("AI ENGINE")
+    text = f"No idea about:\n\t{string}\nPlease Report It To us."
+    detail = 'Unknown'
+    if string:
+        for i in library.keys():
+            if not language in library[i]:
+                language = 'English'
+            if 'detail' in library[i]:
+                try:
+                    detail = library[i]['detail'][language]
+                except (ValueError, KeyError):
+                    detail = 'Unknown'
+            similarity_ = SequenceMatcher(None, i, catch_error).quick_ratio()
+            if similarity_ >= 0.8:
+                text = library[i][language]
+                break
+            else:
+                if similarity_ > similarity:
+                    similarity = similarity_
+                    if similarity < 0.5:
+                        break
+                else:
+                    text = library[i][language]
+                    break
+    f1 = ttk.LabelFrame(window, text=lang.detail)
+    ttk.Label(f1, text=string, font=(None, 12), foreground="orange", wraplength=400).pack(padx=10, pady=5)
+    ttk.Label(f1, text=detail, font=(None, 15),foreground="grey", wraplength=400).pack(padx=10, pady=10)
+    f1.pack(padx=10, pady=10)
+    f2 = ttk.LabelFrame(window, text=lang.solution)
+    ttk.Label(f2, text=text, font=(None, 15),foreground="green", wraplength=400).pack(padx=10, pady=10)
+    f2.pack(padx=10, pady=10)
+
+    ttk.Button(window, text=ok, command=window.destroy, style="Accent.TButton").pack(padx=10, pady=10, fill=BOTH)
+    move_center(window)
---
+++ src/editor.py
@@ -0,0 +1,94 @@
+# Copyright (C) 2024 The MIO-KITCHEN-SOURCE Project
+#
+# Licensed under theGNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html#license-text
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import os
+import time
+import tkinter as tk
+from tkinter import ttk, END, X, LEFT
+from tkinter.ttk import Button
+
+import pygments.lexers
+from chlorophyll import CodeView
+from .utils import create_thread, gettype, lang
+
+
+class PythonEditor(tk.Frame):
+    def __init__(self, parent, path, file_name, lexer=pygments.lexers.BashLexer):
+        tk.Frame.__init__(self, parent)
+        self.file_name = file_name
+        self.path = path
+        if not os.path.exists(self.path):
+            os.makedirs(self.path, exist_ok=True)
+        self.parent = parent
+        self.text = CodeView(self, wrap="word", undo=True, lexer=lexer, color_scheme="dracula")
+        self.text.pack(side="left", fill="both", expand=True)
+        f1 = ttk.Frame(self.parent)
+        ttk.Button(f1, text=lang.text17, command=self.parent.destroy).pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5,
+                                                                              expand=1)
+        self.save_b = ttk.Button(f1, text=lang.t54, command=lambda: create_thread(self.save), style="Accent.TButton")
+        self.save_b.pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5, expand=1)
+        f1.pack(side=tk.BOTTOM, fill=tk.X, padx=5, pady=5)
+        self.show = tk.Listbox(self, activestyle='dotbox', highlightthickness=0)
+        self.show.bind("<Double-Button-1>", lambda x:self.p_bind())
+        self.show.pack(fill=tk.BOTH, padx=5, pady=5)
+        ff = ttk.Frame(self)
+        Button(ff, text=lang.text23, command=self.refs).pack(fill=X, side=LEFT, padx=5, pady=5)
+        ff.pack(padx=5, pady=5, fill=X)
+        self.refs()
+
+    def p_bind(self):
+        try:
+            file = self.show.get(self.show.curselection())
+        except:
+            file = ""
+        var = os.path.abspath(os.path.join(self.path, file))
+        if os.path.isfile(var):
+            self.file_name = os.path.basename(var)
+            self.load()
+
+    def refs(self):
+        self.show.delete(0, END)
+        for f in os.listdir(self.path):
+            if os.path.isfile(os.path.join(self.path, f)):
+                if gettype(os.path.join(self.path, f)) == 'unknown':
+                    self.show.insert(END, f)
+        self.load()
+
+    def save(self):
+        self.save_b.configure(text=lang.t55, state='disabled')
+        with open(os.path.join(self.path, self.file_name), 'w+', encoding='utf-8', newline='\n') as txt:
+            txt.write(self.text.get(1.0, tk.END))
+        time.sleep(1)
+        self.save_b.configure(text=lang.t54, state='normal')
+
+    def load(self):
+        if self.file_name:
+            try:
+                with open(os.path.join(self.path, self.file_name), 'rb+') as f:
+                    self.text.delete(0.0, tk.END)
+                    try:
+                        data = f.read().decode("utf-8")
+                    except:
+                        data = f.read().decode("gbk")
+                    self.text.insert(tk.END, data)
+            except Exception as e:
+                logging.debug(e)
+            self.parent.title(f"{self.file_name} - Editor")
+
+
+def main(file_=None, file_name=None, lexer=pygments.lexers.BashLexer):
+    root = tk.Toplevel()
+    root.title("Editor")
+    editor = PythonEditor(root, file_, file_name, lexer=lexer)
+    editor.pack(side="top", fill="both", expand=True)
